1ab4522226204f9ee83f389728976de1ced64b34
Amrut-17 <amrutpatil9977@gmail.com>
2022-02-13 14:52:42 +0530

    MaterialApp, title, body, scaffold, appBar, debugShowCheckModeBanner, StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,39 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Material(
-      color: Colors.grey,
-      child: Center(
-        child: Text(
-          "Hello Flutter",
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-            color: Colors.orangeAccent,
-            fontSize: 40.0,
-            fontWeight: FontWeight.bold,
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "First App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Amruts First App",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        body: Material(
+          child: Center(
+            child: Text(
+              "Hello Flutter",
+              style: TextStyle(
+                color: Colors.amberAccent,
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.bold,
+                fontSize: 50.0,
+              ),
+            ),
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }

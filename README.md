5c96fcb1a6c1ecad7b9930f219712f260785fb2a
Amrut-17 <amrutpatil9977@gmail.com>
2022-03-05 12:48:38 +0530

    First Page With Slider

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## Concepts :
 ### > Custom Carsousel Slider <br> > TabBar and TabView <br> > ListView.Builder <br> > BottomNavigationBar <br> > Cubit State Management <br> > Understanding Cubit and State <br> > Creating Cubit and States <br> > BlocProvider and BlocBuilder <br> > Cubit Navigation <br> > Cubit Data Load From Sercer <br> > Building Model <br> > Data Repository
 
-### Vertical Scroll --> PageView.builder(scrollDirection: Axis.vertical)
+### First Page With Slider
 <p align="center">
-    <img src="sample.gif"   width="320" height="480" title="hover text">
+    <img src="ss.webm"   width="320" height="480" title="hover text">
 </p>
--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dev_practice/widgets/app_large_text.dart';
+import 'package:flutter_dev_practice/widgets/app_text.dart';
+import 'package:flutter_dev_practice/widgets/responsive_button.dart';
+
+import '../misc/colors.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({Key? key}) : super(key: key);
@@ -22,11 +27,68 @@ class _WelcomePageState extends State<WelcomePage> {
         itemCount: images.length,
         itemBuilder: (context, index) {
           return Container(
+            // CODE FOR BACKGROUND IMAGE START
             width: double.maxFinite,
             height: double.maxFinite,
             decoration: BoxDecoration(
               image: DecorationImage(
-                image: AssetImage("assets/images/" + images[index]),
+                image: AssetImage(
+                  "assets/images/" + images[index],
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+            // CODE FOR BACKGROUND IMAGE END
+            child: Container(
+              // color: Colors.black,
+              margin: const EdgeInsets.only(top: 120, left: 15, right: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      AppLargeText(text: "Trips"),
+                      AppText(text: "Mountain", size: 30),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        width: 250,
+                        // color: Colors.yellow,
+                        child: AppText(
+                          text:
+                              "Mountain hikes give you an incredible sense of freedom along withendurance tests",
+                          color: AppColors.textColor2,
+                          size: 16,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      ResponsiveButton(
+                        width: 120,
+                      ),
+                    ],
+                  ),
+
+                  // 2nd Column start
+                  Column(
+                    children: List.generate(3, (indexDots) {
+                      return Container(
+                        width: 8,
+                        margin: const EdgeInsets.only(bottom: 2),
+                        height: index == indexDots ? 25 : 8,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: index == indexDots
+                              ? AppColors.mainColor
+                              : AppColors.mainColor.withOpacity(0.3),
+                        ),
+                      );
+                    }),
+                  ),
+                ],
               ),
             ),
           );

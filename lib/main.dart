aa1dbe361c496be2d5bbbcb0326365a20be6d127
Amrut-17 <amrutpatil9977@gmail.com>
2022-03-12 19:53:25 +0530

    PageView

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dev_practice/pages/detail_page.dart';
-import 'package:flutter_dev_practice/pages/navpages/main_page.dart';
-import 'package:flutter_dev_practice/pages/welcome_page.dart';
+import 'package:flutter_dev_practice/screens/homePage.dart';
 
 void main() {
   runApp(
@@ -15,12 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Flutter Practice",
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: DetailPage(),
+      home: HomePage(),
     );
   }
 }
